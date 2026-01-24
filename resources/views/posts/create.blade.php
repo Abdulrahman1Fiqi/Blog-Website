05d2106b54dc8a993220d42983a0d5a5ece34d65
Abdulrahman Al_Fiqi <abdulrahmanhmh47@gmail.com>
2026-01-24 08:41:49 +0200

    Making "Create" view

---
+++ resources/views/posts/create.blade.php
@@ -0,0 +1,39 @@
+@extends('layouts.app')
+
+@section('title')
+    Create
+@endsection
+
+@section('Content')
+
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-8">
+            
+            <form>
+                <div class="mb-3">
+                    <label class="form-label">Title</label>
+                    <input type="text" class="form-control">
+                </div>
+
+                <div class="mb-3">
+                    <label class="form-label">Description</label>
+                    <textarea class="form-control" rows="3"></textarea>
+                </div>
+
+                <div class="mb-3">
+                    <label class="form-label">Post Creator</label>
+                    <select class="form-control">
+                        <option value="1">Ahmed</option>
+                        <option value="2">Mohamed</option>
+                    </select>
+                </div>
+
+                <button class="btn btn-success">Submit</button>
+            </form>
+
+        </div>
+    </div>
+</div>
+
+@endsection
