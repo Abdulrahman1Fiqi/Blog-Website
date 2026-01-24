3c740f1114fa51b721ebf21407465dd84ca5022e
Abdulrahman Al_Fiqi <abdulrahmanhmh47@gmail.com>
2026-01-24 08:55:35 +0200

    Handle "Create Post" button

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -7,7 +7,7 @@
 <div class="container mt-5">
     <div class="d-flex justify-content-between align-items-center mb-3">
         <h4>All Posts</h4>
-        <button class="btn btn-success">Create Post</button>
+        <a href="{{ route("posts.create") }}" class="btn btn-success">Create Post</a>
     </div>
 
     <table class="table table-bordered table-striped text-center align-middle">
